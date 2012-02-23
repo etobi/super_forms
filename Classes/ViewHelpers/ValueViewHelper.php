39a233d5d9f2d1977221adf653794b49cf7b0292
Tobias Liebig <tobias.liebig@typo3.org>
2012-02-23 12:20:43 +0100

    [TASK] preset field values after failing validation

--- Classes/ViewHelpers/ValueViewHelper.php
+++ Classes/ViewHelpers/ValueViewHelper.php
@@ -5,19 +5,23 @@ class Tx_SuperForms_ViewHelpers_ValueViewHelper extends Tx_Fluid_Core_ViewHelper
 	 * @param Tx_SuperForms_Domain_Model_Field_Base $field
 	 * @param Tx_SuperForms_Domain_Model_Response $formResponse
 	 * @param Tx_SuperForms_Validation_Result $validationResult
+	 * @param string $trueIfEquals
 	 * @return string
 	 */
-	public function render(Tx_SuperForms_Domain_Model_Field_Base $field, Tx_SuperForms_Domain_Model_Response $formResponse, Tx_SuperForms_Validation_Result $validationResult = NULL) {
+	public function render(Tx_SuperForms_Domain_Model_Field_Base $field, Tx_SuperForms_Domain_Model_Response $formResponse, Tx_SuperForms_Validation_Result $validationResult = NULL, $trueIfEquals = NULL) {
 		$formResponseValues = $formResponse->getValues();
 		$fieldValue = $field->getValue();
-
 		if (!empty($formResponseValues[$field->getName()])) {
-			return $formResponseValues[$field->getName()];
+			$returnValue = $formResponseValues[$field->getName()];
 		} elseif (!empty($fieldValue) && !$validationResult) {
-			return $fieldValue;
+			$returnValue = $fieldValue;
 		} else {
-			return '';
+			$returnValue = '';
 		}
+		if ($trueIfEquals !== NULL) {
+			$returnValue = ($trueIfEquals == $returnValue);
+		}
+		return $returnValue;
 	}
 }
 ?>
